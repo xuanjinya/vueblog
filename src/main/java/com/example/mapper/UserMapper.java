be7d95c8b2c52264dbf4f0929fdf155a6b5d3beb
xuanjinya <1067998193@qq.com>
2020-06-25 09:55:04 +0800

    first commit

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.mapper;
+
+import com.example.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author anonymous
+ * @since 2020-06-25
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
